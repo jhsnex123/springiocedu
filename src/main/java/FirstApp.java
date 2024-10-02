b0f5e10e0c0653d4cfb3af1cf2cafddcb0ac1e43
Han KyuHyun <khh5345@naver.com>
2024-10-02 17:31:05 +0900

    한규현의 첫번째 커밋

--- src/main/java/FirstApp.java
+++ src/main/java/FirstApp.java
@@ -1,6 +1,8 @@
 public class FirstApp {
     public static void main(String[] args) {
         System.out.println("인텔리제이에서 자바프로그래밍을 해요~~");
-        System.out.println("프로젝트 회의");
+
+//        System.out.println("프로젝트 회의");
+        System.out.println("열심히 좀 합시다.");
     }
 }
